53e2e5053b16888373ca46270e8ce843f601255c
PingjunChen <pingjunchen@ieee.org>
2021-02-21 03:11:05 -0600

    update img_feas

--- extract_cell_feas.m
+++ extract_cell_feas.m
@@ -8,8 +8,8 @@ feature_names = {'Area','Perimeter','MajorAxisLength','EquivDiameter','Integrate
 
 for ss = 1:length(subtypes)
     diag = subtypes{ss};
-    disp(['Draw segmentation on ', diag]);
-    cur_diag_fea = struct('name', 'fea');
+    disp(['Extract features from ', diag]);
+    img_feas = struct('name', 'fea');
     cur_diag_dir = fullfile(fea_root, diag);
     img_list = dir(fullfile(cur_diag_dir, '*.mat'));
     for ii = 1:length(img_list)
@@ -21,11 +21,11 @@ for ss = 1:length(subtypes)
             cell_feas(ff,:) = [properties.(feature_names{ff})];
         end
         cell_feas = cell_feas';
-        cur_diag_fea(ii).name = basename;
-        cur_diag_fea(ii).fea = cell_feas;
+        img_feas(ii).name = basename;
+        img_feas(ii).fea = cell_feas;
     end
     cur_diag_fea_path = fullfile(fea_root, strcat(diag, '.mat'));
-    save(cur_diag_fea_path, 'cur_diag_fea');
+    save(cur_diag_fea_path, 'img_feas');
 end
         
     
