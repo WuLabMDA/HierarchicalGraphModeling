0611cfbf3690f5489166a09407240614c0b755d5
PingjunChen <pingjunchen@ieee.org>
2021-02-21 01:12:44 -0600

    add extract cell features

--- extract_cell_feas.m
+++ extract_cell_feas.m
@@ -1,29 +1,31 @@
 clearvars;
-
-% add nuclei segmentation package
-addpath(genpath('./nuclei_seg'));
-
-% set folders
-img_root = fullfile('./data', 'LymphImgs');
 fea_root = fullfile('./data', 'ImgCellFeas');
+
 subtypes = {'CLL', 'aCLL', 'RT'};
+feature_names = {'Area','Perimeter','MajorAxisLength','EquivDiameter','IntegratedIntensity',...
+    'MinorAxisLength','MeanOutsideBoundaryIntensity','NormalizedBoundarySaliency',...
+    'NormalizedOutsideBoundaryIntensity','MeanInsideBoundaryIntensity'};
 
 for ss = 1:length(subtypes)
     diag = subtypes{ss};
-    disp(['Extract ', diag]);
-    cur_img_dir = fullfile(img_root, diag);
-    cur_fea_dir = fullfile(fea_root, diag);
-    img_list = dir(fullfile(cur_img_dir, '*.png'));
+    disp(['Draw segmentation on ', diag]);
+    cur_diag_fea = struct('name', 'fea');
+    cur_diag_dir = fullfile(fea_root, diag);
+    img_list = dir(fullfile(cur_diag_dir, '*.mat'));
     for ii = 1:length(img_list)
-        disp([num2str(ii), '/', num2str(length(img_list))]);
-        cur_img_path = fullfile(cur_img_dir, img_list(ii).name);
-        I = imread(cur_img_path);
-        [I_norm, ~, ~] = normalizeStaining(I);
-        I_normRed=I_norm(:,:,1);
-        p.scales=3:2:10; % the scale of nuclei
-        [nuclei, properties] = nucleiSegmentationV2(I_normRed, p); 
         [~, basename, ~] = fileparts(img_list(ii).name);
-        cur_fea_path = fullfile(cur_fea_dir, strcat(basename, '.mat'));
-        save(cur_fea_path, 'I', 'nuclei', 'properties');
+        cur_fea_path = fullfile(cur_diag_dir, img_list(ii).name);
+        load(cur_fea_path, 'properties');
+        cell_feas = zeros(length(feature_names), length(properties));
+        for ff=1:length(feature_names)
+            cell_feas(ff,:) = [properties.(feature_names{ff})];
+        end
+        cell_feas = cell_feas';
+        cur_diag_fea(ii).name = basename;
+        cur_diag_fea(ii).fea = cell_feas;
     end
+    cur_diag_fea_path = fullfile(fea_root, strcat(diag, '.mat'));
+    save(cur_diag_fea_path, 'cur_diag_fea');
 end
+        
+    
