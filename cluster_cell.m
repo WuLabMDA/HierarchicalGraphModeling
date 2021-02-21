9ff507c7698d4dc77adbee6ffecf8eba9cc9917a
PingjunChen <pingjunchen@ieee.org>
2021-02-21 13:02:39 -0600

    cell cluster using t-SNE & spectral clustering

--- cluster_cell.m
+++ cluster_cell.m
@@ -17,4 +17,10 @@ chosen_idx = randperm(size(all_feas, 1), chosen_num);
 chosen_feas = all_feas(chosen_idx, :);
 
 % perform t-sne
+t_feas = tsne(chosen_feas, 'Perplexity', 50, 'Standardize', true);
+scatter(t_feas(:, 1), t_feas(:, 2), 'filled');
+title('2D t-SNE Embedding');
+
 % perform spectral clustering to have 2 cluster
+[ids, ~, ~] = spectralcluster(t_feas, 2);
+gscatter(t_feas(:, 1), t_feas(:, 2), ids, 'rg', '', [15, 15]);
