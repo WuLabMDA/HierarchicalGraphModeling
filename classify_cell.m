a8c7d75433b6eecd5fc915cb27f7b70bf1a38536
PingjunChen <pingjunchen@ieee.org>
2021-02-21 13:03:27 -0600

    build cell classifier

--- classify_cell.m
+++ classify_cell.m
@@ -28,6 +28,7 @@ all_acll_feas = cell2mat(all_acll_feas);
 chose_acll_idx = randperm(size(all_acll_feas, 1), chose_num);
 chose_acll_feas = all_acll_feas(chose_acll_idx, :);
 clear img_feas all_acll_feas chose_acll_idx;
+
 % select features from RT
 rt_fea_path = fullfile(fea_root, 'RT.mat');
 load(rt_fea_path);
@@ -40,14 +41,28 @@ all_rt_feas = cell2mat(all_rt_feas);
 chose_rt_idx = randperm(size(all_rt_feas, 1), chose_num);
 chose_rt_feas = all_rt_feas(chose_rt_idx, :);
 clear img_feas all_rt_feas chose_rt_idx;
+
 % combine data
 combine_data = cat(1, chose_cll_feas, chose_acll_feas, chose_rt_feas);
 %t-sne
 %spectral-clustering to obtain labels;
 
+t_feas = tsne(combine_data, 'Perplexity', 50, 'Standardize', true);
+[labels, ~, ~] = spectralcluster(t_feas, 2);
+% gscatter(t_feas(:, 1), t_feas(:, 2), labels, 'rg', '', [15, 15]);
 
-[norm_data, fea_mu, fea_sd] = zscore(combine_data); 
+
+id1_set = find(labels == 1);
+id2_set = find(labels == 2);
+id1_set_idx = randperm(size(id1_set, 1), 2100);
+id2_set_idx = randperm(size(id2_set, 1), 900);
+select_idx = cat(2, id1_set_idx, id2_set_idx);
+select_combine_data = combine_data(select_idx, :);
+select_labels = labels(select_idx, :);
+
+[norm_data, fea_mu, fea_sd] = zscore(select_combine_data); 
 % Build classification model
-cell_clf_model = fitcensemble(norm_data, labels, 'Method', 'Bag');
+cell_clf_model = fitcensemble(norm_data, select_labels, 'Method', 'Bag');
 % Save the cell 
-save('cell_clf_para.mat', 'cell_clf_model', 'fea_mu', 'fea_sd');
+cell_clf_para_path = fullfile('./data', 'Models', 'cell_clf_para.mat');
+save(cell_clf_para_path, 'cell_clf_model', 'fea_mu', 'fea_sd');
