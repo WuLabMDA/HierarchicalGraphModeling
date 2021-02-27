bc1a90ccfd72d0f98ba4c249f6c5c48366a821a2
PingjunChen <pingjunchen@ieee.org>
2021-02-27 12:20:21 -0600

    perform the KFold SVM classification

--- img_graph_cls.m
+++ img_graph_cls.m
@@ -4,79 +4,47 @@ rng('default');
 img_edge_fea_path = fullfile('./data', 'GlobalGraph', 'graph_fea.mat');
 load(img_edge_fea_path, 'population_img_feas');
 
-test_ratio = 0.2;
-graph_fea_num = 4;
-num_boot_strap = 100;
-
-cll_feas = zeros(length(population_img_feas(1).feas), graph_fea_num);
-for ff=1:length(cll_feas)
-    cll_feas(ff, :) = population_img_feas(1).feas(ff).graph_feas(1:graph_fea_num);
+fea_num = 15;
+save(img_edge_fea_path, 'population_img_feas');
+cll_graph_info = population_img_feas(1).feas;
+acll_graph_info = population_img_feas(2).feas;
+rt_graph_info = population_img_feas(3).feas;
+cll_graph_feas = zeros(20, fea_num);
+acll_graph_feas = zeros(20, fea_num);
+rt_graph_feas = zeros(20, fea_num);
+for ii = 1:20
+    cll_graph_feas(ii, :) = cll_graph_info(ii).graph_feas;
+    acll_graph_feas(ii, :) = acll_graph_info(ii).graph_feas;
+    rt_graph_feas(ii, :) = rt_graph_info(ii).graph_feas;
 end
-acll_feas = zeros(length(population_img_feas(2).feas), graph_fea_num);
-for ff=1:length(acll_feas)
-    acll_feas(ff, :) = population_img_feas(2).feas(ff).graph_feas(1:graph_fea_num);
-end
-rt_feas = zeros(length(population_img_feas(3).feas), graph_fea_num);
-for ff=1:length(rt_feas)
-    rt_feas(ff, :) = population_img_feas(3).feas(ff).graph_feas(1:graph_fea_num);
-end
+labels = zeros(60, 1);
+labels(1:20) = 1;
+labels(21:40) = 2;
+labels(41:60) = 3;
+all_feas = zeros(60, fea_num);
+all_feas(1:20, :) = cll_graph_feas;
+all_feas(21:40, :) = acll_graph_feas;
+rt_feas(41:60, :) = rt_graph_feas;
 
-train_acc_list = zeros(num_boot_strap, 1);
-test_acc_list = zeros(num_boot_strap, 1);
-for bb = 1:num_boot_strap
-    cll_labels = ones(length(cll_feas), 1);
-    cll_cv = cvpartition(size(cll_feas,1),'HoldOut', test_ratio);
-    idx = cll_cv.test;
-    cll_train_feas = cll_feas(~idx, :);
-    cll_train_labels = cll_labels(~idx, :);
-    cll_test_feas = cll_feas(idx, :);
-    cll_test_labels = cll_labels(idx, :);
+data_partition = cvpartition(labels,'KFold', 5); % Create a random partition for stratified 5-fold cross-validation.
+svm_cv_model = fitcecoc(all_feas, labels, 'CVPartition', data_partition, ...
+    'Learners', templateSVM('Standardize',true), 'ClassNames', {'1','2','3'});
+svm_errs = kfoldLoss(svm_cv_model, 'mode','individual');
+acc_rates = 1.0 - svm_errs;
+disp(['The cross valiation mean acc: ', num2str(mean(acc_rates))]);
+disp(['The cross valiation std acc: ', num2str(std(acc_rates))]);
 
-    acll_labels = ones(size(acll_feas, 1), 1) * 2;
-    acll_cv = cvpartition(size(acll_feas,1),'HoldOut', test_ratio);
-    idx = acll_cv.test;
-    acll_train_feas = acll_feas(~idx, :);
-    acll_train_labels = acll_labels(~idx, :);
-    acll_test_feas = acll_feas(idx, :);
-    acll_test_labels = acll_labels(idx, :);
 
-    rt_labels = ones(size(rt_feas, 1), 1) * 3;
-    rt_cv = cvpartition(size(rt_feas,1),'HoldOut', test_ratio);
-    idx = rt_cv.test;
-    rt_train_feas = rt_feas(~idx, :);
-    rt_train_labels = rt_labels(~idx, :);
-    rt_test_feas = rt_feas(idx, :);
-    rt_test_labels = rt_labels(idx, :);
+%% Cell Voronoi
+%% Cell Flock-Centroid
+%% Cell Flock-AreaIntensity
 
-    train_feas = [cll_train_feas; acll_train_feas; rt_train_feas];
-    train_labels = [cll_train_labels; acll_train_labels; rt_train_labels];
-    test_feas = [cll_test_feas; acll_test_feas; rt_test_feas];
-    test_labels = [cll_test_labels; acll_test_labels; rt_test_labels];
-    
-    % build classifier
-    % Bootstrap Aggregation (Bagging) 
-    % img_clf_model = fitcensemble(train_feas, train_labels, 'Method', 'Bag');
-    % Fit multiclass models for support vector machines
-    img_clf_model = fitcecoc(train_feas, train_labels);
-    % Fit discriminant analysis classifier
-    % img_clf_model = fitcdiscr(train_feas, train_labels);
-
-    % evaluate on the train
-    pred_labels = predict(img_clf_model, train_feas);
-    train_conf_mat = confusionmat(train_labels, pred_labels);
-    train_acc = trace(train_conf_mat) / sum(train_conf_mat, 'all');
-    disp(['Train acc: ', num2str(train_acc)]);
-    train_acc_list(bb) = train_acc;
-        
-    % evaluate on the test
-    pred_labels = predict(img_clf_model, test_feas);
-    test_conf_mat = confusionmat(test_labels, pred_labels);
-    test_acc = trace(test_conf_mat) / sum(test_conf_mat, 'all');
-    disp(['Test acc: ', num2str(test_acc)]);
-    test_acc_list(bb) = test_acc;
-end
-
-disp(['Mean train acc is: ', num2str(mean(train_acc_list)), '+', ...
-    num2str(std(train_acc_list))]);
-disp(['Mean test acc is: ', num2str(mean(test_acc_list)), '+', ...
-    num2str(std(test_acc_list))]);
+% %% Supercell Delaynay
+% The cross valiation mean acc: 0.83333
+% The cross valiation std acc: 0.058926
+% %% Voronoi
+% The cross valiation mean acc: 0.78333
+% The cross valiation std acc: 0.095015
+% %% Delaynay + Voronoi
+% The cross valiation mean acc: 0.86667
+% The cross valiation std acc: 0.045644
