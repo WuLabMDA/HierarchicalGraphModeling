41bb2a3e06cbb06c2c42b2b85dd9c5da5ee6ee82
PingjunChen <pingjunchen@ieee.org>
2021-02-28 23:54:18 -0600

    add bootstrap classification

--- img_graph_cls.m
+++ img_graph_cls.m
@@ -1,11 +1,8 @@
 clearvars;
 rng('default');
-
 img_edge_fea_path = fullfile('./data', 'GlobalGraph', 'graph_fea.mat');
 load(img_edge_fea_path, 'population_img_feas');
-
 fea_num = 15;
-save(img_edge_fea_path, 'population_img_feas');
 cll_graph_info = population_img_feas(1).feas;
 acll_graph_info = population_img_feas(2).feas;
 rt_graph_info = population_img_feas(3).feas;
@@ -24,13 +21,19 @@ labels(41:60) = 3;
 all_feas = zeros(60, fea_num);
 all_feas(1:20, :) = cll_graph_feas;
 all_feas(21:40, :) = acll_graph_feas;
-rt_feas(41:60, :) = rt_graph_feas;
+all_feas(41:60, :) = rt_graph_feas;
 
-data_partition = cvpartition(labels,'KFold', 5); % Create a random partition for stratified 5-fold cross-validation.
-svm_cv_model = fitcecoc(all_feas, labels, 'CVPartition', data_partition, ...
-    'Learners', templateSVM('Standardize',true), 'ClassNames', {'1','2','3'});
-svm_errs = kfoldLoss(svm_cv_model, 'mode','individual');
-acc_rates = 1.0 - svm_errs;
+rand_times = 100;
+acc_rates = zeros(rand_times, 1);
+for rr=1:rand_times
+    data_partition = cvpartition(labels,'KFold', 5); % Create a random partition for stratified 5-fold cross-validation.
+    svm_cv_model = fitcecoc(all_feas, labels, 'CVPartition', data_partition, ...
+        'Learners', templateSVM('Standardize',true), 'ClassNames', {'1','2','3'});
+    svm_errs = kfoldLoss(svm_cv_model, 'mode','individual');
+    svm_accs = 1.0 - svm_errs;
+    cur_acc = mean(svm_accs);
+    acc_rates(rr) = cur_acc;
+end
 disp(['The cross valiation mean acc: ', num2str(mean(acc_rates))]);
 disp(['The cross valiation std acc: ', num2str(std(acc_rates))]);
 
