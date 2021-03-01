4e86bead60be8c267fb76f72d097443ae280ea85
PingjunChen <pingjunchen@ieee.org>
2021-02-28 23:51:38 -0600

    crop image for demo

--- demo/crop_img_seg.m
+++ demo/crop_img_seg.m
@@ -8,9 +8,12 @@ overlay_root = fullfile('./demo', 'Overlays');
 img_list = dir(fullfile(img_root, '*.png'));
 for ii = 1:length(img_list)
     cur_img_path = fullfile(img_root, img_list(ii).name);
+    [~, basename, ~] = fileparts(img_list(ii).name);
     I = imread(cur_img_path);
-    I = I(251:750, 251:750, 1:end);
+    I = I(351:650, 251:750, 1:end);
+    raw_img_path = fullfile(overlay_root, strcat(basename, '_raw.png'));
     [I_norm, ~, ~] = normalizeStaining(I); 
+    imwrite(I_norm, raw_img_path);
     I_normRed=I_norm(:,:,1);
     p.scales=3:2:10; % the scale of nuclei
     [nuclei, properties] = nucleiSegmentationV2(I_normRed, p); 
@@ -21,8 +24,8 @@ for ii = 1:length(img_list)
         plot(nuclei{k}(:,2), nuclei{k}(:,1), 'r-', 'LineWidth', 1);
     end
     hold off;     
-    [~, basename, ~] = fileparts(img_list(ii).name);
-    overlay_img_path = fullfile(overlay_root, strcat(basename, '_central.png'));
+    
+    overlay_img_path = fullfile(overlay_root, strcat(basename, '_cell.png'));
     imwrite(getframe(gca).cdata, overlay_img_path);
     close all;
 end
