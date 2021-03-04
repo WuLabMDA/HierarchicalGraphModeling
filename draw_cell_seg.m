f99681bb591e8a28efb083210fdc1c6a0eb2227c
PingjunChen <pingjunchen@ieee.org>
2021-03-04 14:24:26 -0600

    add folder make

--- draw_cell_seg.m
+++ draw_cell_seg.m
@@ -4,7 +4,7 @@ figure('visible', 'off')
 % set folders
 img_root = fullfile('./data', 'LymphImgs');
 fea_root = fullfile('./data', 'ImgCellFeas');
-overlay_root = fullfile('./data', 'CellOverlay');
+overlay_root = fullfile('./data', 'NucleusOverlay');
 subtypes = {'CLL', 'aCLL', 'RT'};
 
 for ss = 1:length(subtypes)
@@ -12,14 +12,20 @@ for ss = 1:length(subtypes)
     disp(['Draw segmentation on ', diag]);
     cur_img_dir = fullfile(img_root, diag);
     cur_fea_dir = fullfile(fea_root, diag);
+    cur_overlay_dir = fullfile(overlay_root, diag);
+    if ~exist(cur_overlay_dir, 'dir')
+        mkdir(cur_overlay_dir)
+    end    
+    
     img_list = dir(fullfile(cur_img_dir, '*.png'));
     for ii = 1:length(img_list)
         disp([num2str(ii), '/', num2str(length(img_list))]);
         cur_img_path = fullfile(cur_img_dir, img_list(ii).name);
         I = imread(cur_img_path);
-        [~, basename, ~] = fileparts(img_list(ii).name);
+        [~, basename, ~] = fileparts(img_list(ii).name);  
         cur_fea_path = fullfile(cur_fea_dir, strcat(basename, '.mat'));
         load(cur_fea_path, 'nuclei');
+        
         % draw cells
         imshow(I);
         hold on;
@@ -27,7 +33,7 @@ for ss = 1:length(subtypes)
             plot(nuclei{k}(:,2), nuclei{k}(:,1), 'r-', 'LineWidth', 1);
         end
         hold off;     
-        overlay_img_path = fullfile(overlay_root, diag, strcat(basename, '.png'));
+        overlay_img_path = fullfile(cur_overlay_dir, strcat(basename, '.png'));
         imwrite(getframe(gca).cdata, overlay_img_path);
     end
 end
