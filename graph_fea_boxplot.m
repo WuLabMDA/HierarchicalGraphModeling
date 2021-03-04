249e934e36e0a5327b0634226f36a4988ab733c5
PingjunChen <pingjunchen@ieee.org>
2021-03-04 15:18:54 -0600

    add graph_fea_boxplot

--- graph_fea_boxplot.m
+++ graph_fea_boxplot.m
@@ -11,24 +11,18 @@ for ss = 1:length(subtypes)
     ss_list = zeros(length(feas), 1);
     sl_list = zeros(length(feas), 1);
     ll_list = zeros(length(feas), 1);
-%     c1_list = zeros(length(feas), 1);
-%     c2_list = zeros(length(feas), 1);
     for ii=1:length(feas)
         ss_list(ii) = feas(ii).graph_feas(1);
         sl_list(ii) = feas(ii).graph_feas(2);
         ll_list(ii) = feas(ii).graph_feas(3);
-%         c1_list(ii) = feas(ii).graph_feas(4);
-%         c2_list(ii) = feas(ii).graph_feas(5);
     end
     
-%     boxplot([ss_list, sl_list, ll_list, c1_list, c2_list], ...,
-%         'Notch', 'on', 'Labels',{'SS', 'SL', 'LL', 'C1', 'C2'});
     boxplot([ss_list, sl_list, ll_list], ...,
         'Notch', 'on', 'Labels',{'SS', 'SL', 'LL'});
     ylim([0.0 1.0]);
     title(subtypes{ss});
 end
 suptitle('Edge Connection Ratio Boxplot');
-fig_save_path = fullfile('./data', 'Demos', 'GraphFeas', 'graph_feas.png');
+fig_save_path = fullfile('./data', 'GlobalGraph', 'graph_feas.png');
 imwrite(getframe(gcf).cdata, fig_save_path);
 close all;
